f981ed40117af267aab1a30f93fc46f09de31db7
ajmalpoovanath <ajmal.poovanath@gmail.com>
2024-07-15 09:50:15 +0530

    Initial Commit

---
+++ Menu/MenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuApp.swift
+//  Menu
+//
+//  Created by Ajmal p on 15/07/24.
+//
+
+import SwiftUI
+
+@main
+struct MenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
