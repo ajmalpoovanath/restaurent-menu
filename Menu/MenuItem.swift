03b9e3b10548d3550bed54128e2c045d1aff7ed7
ajmalpoovanath <ajmal.poovanath@gmail.com>
2024-07-16 07:51:02 +0530

    Structure created

---
+++ Menu/MenuItem.swift
@@ -0,0 +1,14 @@
+//
+//  MenuItem.swift
+//  Menu
+//
+//  Created by Ajmal p on 16/07/24.
+//
+
+import Foundation
+
+struct MenuItem {
+    var name: String
+    var prince: String
+    var imageName: String
+}
---
+++ Menu/MenuItems.swift
@@ -0,0 +1,10 @@
+//
+//  MenuItems.swift
+//  Menu
+//
+//  Created by Ajmal p on 16/07/24.
+//
+
+import Foundation
+
+struct MenuItem
